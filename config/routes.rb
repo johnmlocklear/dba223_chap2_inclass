c9aca28912df1be043d541788d2dd89657733b23
Mark Locklear <marklocklear@gmail.com>
2014-09-02 16:39:37 -0400

    init6

--- config/routes.rb
+++ config/routes.rb
@@ -51,8 +51,8 @@ Inclass::Application.routes.draw do
 
   # See how all your routes lay out with "rake routes"
 
-  root :to => 'home#index'
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   match ':controller(/:action(/:id))(.:format)'
+  root :to => 'home#index'
 end
